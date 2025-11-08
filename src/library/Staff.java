bf00b69c67c00301e692537230594c0c2213fbbe
SalvadorB-2 <barrigas252@gmail.com>
2025-11-07 18:36:56 -0800

    added counter for unique ids, added constructor, added getters

--- src/library/Staff.java
+++ src/library/Staff.java
@@ -6,4 +6,20 @@ import java.util.EnumSet;
 public class Staff {
     // Staff class implementation - member variables and methods to be determined based on application needs
 	String name;
+	String uid;
+	static int idCounter = 0;
+	
+	public Staff(String Name) {
+		name = Name;
+		idCounter++;
+		uid = "SID" + idCounter;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getUID() {
+		return uid;
+	}
 }
