4b88bc5a1ef67d469ddbfdb042eb1393b01af1c0
SalvadorB-2 <barrigas252@gmail.com>
2025-11-08 13:19:30 -0800

    added role

--- src/library/Staff.java
+++ src/library/Staff.java
@@ -7,12 +7,13 @@ public class Staff {
     // Staff class implementation - member variables and methods to be determined based on application needs
 	private String name;
 	private String uid;
+	private String role;
 	private static int idCounter = 0; //for unique ids
 	
-	public Staff(String Name) {
+	public Staff(String Name, String Role) {
 		name = Name;
-		idCounter++;
-		uid = "SID" + idCounter;
+		role = Role;
+		uid = "SID" + ++idCounter;
 	}
 	
 	public String getName() {
