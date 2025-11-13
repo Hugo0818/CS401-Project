e1f12c4e30fd55db129e52c0eff8f94214d1c666
Ryan-pelo <xfactor529@gmail.com>
2025-11-13 13:41:15 -0800

    change clientMain to clientApp to avoid confusion, Clientapp makes a new GUI obj

---
+++ src/client/ClientApp.java
@@ -0,0 +1,9 @@
+package client;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        GUIManager gui = new GUIManager();
+        // For now, just instantiate the GUIManager
+        // the GUIManager automatically makes a client object and connects to the server
+    }
+}
